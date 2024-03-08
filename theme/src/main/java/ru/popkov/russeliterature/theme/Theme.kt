787a58db150d6f3921aed17f4ad1e55dd7c15c21
Denis Popkov <popkov-den03@mail.ru>
2024-03-08 10:09:11 +0600

    remove: dynamic color support

--- theme/src/main/java/ru/popkov/russeliterature/theme/Theme.kt
+++ theme/src/main/java/ru/popkov/russeliterature/theme/Theme.kt
@@ -1,18 +1,14 @@
 package ru.popkov.russeliterature.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import ru.popkov.russeliterature.theme.Colors.Pink40
 import ru.popkov.russeliterature.theme.Colors.Pink80
@@ -36,18 +32,9 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
